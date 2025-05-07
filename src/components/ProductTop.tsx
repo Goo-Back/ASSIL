b3f543a57bf2601ed0d0bc76fd333e5feee660b3
Goo-Back <yasserluis584@gmail.com>
2025-05-07 16:21:24 +0200

    Update ProductTop.tsx

--- src/components/ProductTop.tsx
+++ src/components/ProductTop.tsx
@@ -17,7 +17,7 @@ const products = [
     rating: 5,
     originalPrice: 30,
     currentPrice: 15,
-    image: '../../public/images/assil.jpg',
+    image: '/images/assil.jpg',
     pdf:'/shawarma.pdf',
   },
   {
@@ -46,7 +46,7 @@ const BestSellers = () => {
   return (
     <section className="py-16 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-center text-gray-900 mb-12">TOP VENTES</h2>
+        <h2 className="text-3xl font-bold text-center text-gray-900 mb-12">VENTES</h2>
         
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {products.map((product) => (
