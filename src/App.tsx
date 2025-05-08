e2209b7ce2869c27ba4d64323e632b7561c824de
Goo-Back <yasserluis584@gmail.com>
2025-05-08 19:25:57 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,24 @@ import Contact from "./components/Contact"
 import Epices from "./components/Epices"
 import Home from "./components/Home"
 import Page404 from "./components/Page404"
+
 function App() {
   return (
-    <BrowserRouter basename="/assil">
       <div>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/ASSIL" element={<Home />} />
-          <Route path="/" element={<Home />} />
           <Route path="/Offre" element={<Offre />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Epices" element={<Epices />} />
-          <Route path="/*" element={<Page404 />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
         <Footer />
       </div>
-    </BrowserRouter>
   );
 }
+
 
 export default App
